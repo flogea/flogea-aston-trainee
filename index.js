86bf22660805dad8f1f17eb272c5d6b3fac485dc
flogea <katua.logvinova@gmail.com>
2023-09-23 14:07:38 +0300

    homeWork_03

--- index.js
+++ index.js
@@ -1,101 +1,23 @@
 // №1
-const counter = {};
-const counter1 = Object.create(null);
-const counter3 = Object.assign({}, {});
+
+// На самом деле массивы в JavaScript являются “неправильными” и совмещают в себе функции сразу нескольких структур
+// данных. По задумке длина массива должна быть неизменной. Для того, чтобы добавить элемент в массив, нужно создать новый
+// массив длиннее старого на 1 элемент, затем скопировать в него все значения старого массива и в качестве последнего элемента
+// указать новое значение.
 
 // №2
-const copyObj = () => {
-  const obj1 = { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5 };
-  const obj2 = {};
 
-  for (const key in obj1) {
-    if (Object.hasOwnProperty.call(obj1, key)) {
-      const element = obj1[key];
-      obj2[key] = element;
-    }
-  }
-  return obj2;
-};
-copyObj();
+function logger() {
+  console.log(`I output only external context: ${this.item}`);
+}
 
-const copyObjv2 = () => {
-  const obj1 = { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5 };
-  const obj2 = {};
+const obj1 = { item: 'some value call' };
+const obj2 = { item: 'some value apply' };
+const obj3 = { item: 'some value bind' };
 
-  Object.assign(obj2, obj1);
-  console.log(obj2);
-  return obj2;
-};
-copyObjv2();
+logger.call(obj1); // some value
+logger.apply(obj2); // some value
+const loggerWithBind = logger.bind(obj3);
+loggerWithBind(); // some value
 
 // №3
-
-function makeCounter() {}
-
-const makeCounter = () => {};
-
-const makeCounter = function () {};
-
-const makeCounter = function newMakeCounter() {};
-
-// Bonus #1
-// Написать функцию глубокого сравнения двух объектов:
-
-const obj1 = { here: { is: 'on', other: '3' }, object: 'Y' };
-const obj2 = { here: { is: 'on', other: '2' }, object: 'Y' };
-
-const deepEqual = (obj1, obj2) => {
-  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
-    return obj1 === obj2;
-  }
-
-  if (obj1 === null && obj2 === null) {
-    return true;
-  }
-
-  if (obj1 === null || obj2 === null) {
-    return false;
-  }
-
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return false;
-  }
-
-  for (let key of Object.keys(obj1)) {
-    if (!Object.keys(obj2).includes(key)) {
-      return false;
-    }
-
-    if (!deepEqual(obj1[key], obj2[key])) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-// Bonus #2
-// Развернуть строку в обратном направлении при помощи методов массивов:
-
-function reverseStr(str) {
-  let arr = [];
-  for (let i = 0; i < str.length; i++) {
-    arr.unshift(str[i]);
-  }
-  return arr.join('');
-}
-reverseStr('Привет');
-
-function reverseStr2(str) {
-  let arr = [];
-  for (let i = 0; i < str.length; i++) {
-    arr.push(str[i]);
-  }
-  return arr.reverse().join('');
-}
-reverseStr2('Привет');
-
-function reverseStr3(str) {
-  return str.split('').reverse().join('');
-}
-reverseStr3('Привет');
